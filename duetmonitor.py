ac7ee621b9e6ec18f2558cf50b9d5a228bd0bd8d
jasoncross <jason@jasoncross.com>
2019-07-31 08:44:51 -0500

    Image resizing

--- duetmonitor.py
+++ duetmonitor.py
@@ -110,9 +110,9 @@ def getImage():
             handler.write(img_data)
         img = Image.open(printimage)
         wpercent = (basewidth / float(img.size[0]))
-         hsize = int((float(img.size[1]) * float(wpercent)))
-         img = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
-         img.save(printimage)
+        hsize = int((float(img.size[1]) * float(wpercent)))
+        img = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
+        img.save(printimage)
         files = {
             "attachment": ("printimage.jpg", open(printimage, "rb"), "image/jpeg")
         }
